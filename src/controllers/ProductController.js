7c4d819d16ec39d6740867ab8f71e1693c77b388
Felipe Trova <felipee.trova@gmail.com>
2020-04-12 19:31:03 -0300

    Adicionando funcao de CRUD

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -2,15 +2,38 @@ const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
 module.exports = {
+  // List all products
   async index(req, res) {
     const products = await Product.find();
-    
     return res.json(products);
   },
 
+  // Show product by Id
+  async show(req, res) {
+    const product = await Product.findById(req.params.id);
+    return res.json(product);
+  },
+
+  // Create a product
   async store(req, res) {
-    // Create
     const product = await Product.create(req.body);
     return res.json(product);
-  }
+  },
+
+  // Update a product by Id
+  async update(req, res) {
+    const product = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true
+      }
+    );
+    return res.json(product);
+  },
+
+  async destroy(req, res) {
+    await Product.findByIdAndRemove(req.params.id);
+    return res.send();
+  },
 };
